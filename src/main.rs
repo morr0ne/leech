32eff2a98bd3f0fe5c0c3e8a91431985a706deb4
Federico Morrone <contact@morrone.dev>
2020-12-07 20:51:23 +0100

    Separeted client logic and added some comments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use anyhow::Result;
 use bytes::{Buf, BufMut, BytesMut};
+use clap::{App, Arg};
 use rand::random;
-use tokio::fs;
-use tokio::net::UdpSocket;
+use std::net::{Ipv4Addr, SocketAddrV4};
+use tokio::{
+    fs,
+    net::{TcpStream, UdpSocket},
+};
 use url::Url;
 
+pub mod client;
 pub mod meta_info;
 
-use meta_info::MetaInfo;
+pub use client::Client;
 
-pub const CONNECT: i32 = 0;
-pub const ANNOUNCE: i32 = 1;
+use meta_info::MetaInfo;
 
 pub const NONE: i32 = 0;
 pub const COMPLETED: i32 = 1;
@@ -19,45 +23,43 @@ pub const STOPPED: i32 = 3;
 
 pub const NUM_WANT: i32 = -1;
 
+pub const CHOKE: i32 = 0;
+pub const UNCHOKE: i32 = 1;
+pub const INTERESTED: i32 = 2;
+pub const NOT_INTERESTED: i32 = 3;
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let t = fs::read("manjaro.torrent").await?;
+    // Initialize cli application
+    let matches = App::new("leech")
+        .version("0.1.0")
+        .arg(Arg::with_name("torrent").required(true))
+        .get_matches();
 
+    // Get path to the torrent
+    let torrent = matches.value_of("torrent").unwrap();
+
+    println!("Parsing torrent");
+
+    // Read torrent and parse meta_info
+    let t = fs::read(torrent).await?;
     let meta_info: MetaInfo = serde_bencode::from_bytes(&t)?;
+
+    // Get announce tracker and make sure it's a valid url
     let tracker = Url::parse(&meta_info.announce)?;
 
+    // Build the tracker url using ip and port
     let url = format!(
         "{}:{}",
         tracker.host_str().unwrap(),
         tracker.port().unwrap()
     );
 
-    let socket = UdpSocket::bind("0.0.0.0:0").await?;
-    socket.connect(&url).await?;
+    println!("Connecting to {}", &url);
 
-    const PROTOCOL_ID: i64 = 0x41727101980;
-    let transaction_id = random::<i32>();
-
-    let mut connect_req = BytesMut::with_capacity(16);
-
-    connect_req.put_i64(PROTOCOL_ID);
-    connect_req.put_i32(CONNECT);
-    connect_req.put_i32(transaction_id);
-
-    socket.send(&connect_req).await?;
-
-    let mut connect_res = BytesMut::with_capacity(65508);
-    connect_res.resize(65508, 0);
-
-    let n = socket.recv(&mut connect_res).await?;
-
-    println!("Received {} bytes", n);
-
-    let _action = connect_res.get_i32();
-    let _transaction_id = connect_res.get_i32();
-    let connection_id = connect_res.get_i64();
-
-    let transaction_id = random::<i32>();
+    // Initialize bittorent client
+    let client = Client::new().await?;
+    let connect_res = client.connect(&url).await?;
 
     let info_hash: [u8; 20] = meta_info.info_hash()?;
 
@@ -65,56 +67,33 @@ async fn main() -> Result<()> {
 
     let left = match meta_info.info {
         meta_info::Info::SingleFile { length, .. } => length,
-        meta_info::Info::MultiFile { files, .. } => {
+        meta_info::Info::MultiFile { ref files, .. } => {
             files.iter().fold(0, |index, file| index + file.length)
         }
     };
 
-    let key = random::<i32>();
+    println!("Sending announce request");
 
-    let mut announce_req = BytesMut::with_capacity(98);
+    // Send announce request
+    let announce_res = client
+        .announce(connect_res.connection_id, info_hash, left)
+        .await?;
 
-    announce_req.put_i64(connection_id);
-    announce_req.put_i32(ANNOUNCE);
-    announce_req.put_i32(transaction_id);
-    announce_req.put_slice(&info_hash);
-    announce_req.put_slice(&peer_id);
-    announce_req.put_i64(0); // downloaded
-    announce_req.put_i64(left);
-    announce_req.put_i64(0); // uploaded
-    announce_req.put_i32(NONE);
-    announce_req.put_i32(0); // ip address
-    announce_req.put_i32(key);
-    announce_req.put_i32(NUM_WANT);
-    announce_req.put_i16(6881); // port should be between 6881 and 6889
+    // Create tcp connection and build handshake
+    // If the connection is refused it probably means this peer is no good
+    // In a proper client you'd want to connect to as many peers as possible 
+    // but for the sake of simplicity I'll connect just to one for now
 
-    socket.send(&announce_req).await?;
+    let mut stream = TcpStream::connect(announce_res.peers[0]).await?;
 
-    let mut announce_res = BytesMut::with_capacity(65508);
-    announce_res.resize(65508, 0);
+    let mut handshake = BytesMut::with_capacity(68);
 
-    let n = socket.recv(&mut announce_res).await?;
-    announce_res.truncate(n);
-
-    println!("Received {} bytes", n);
-
-    let action = announce_res.get_i32();
-    let transaction_id = announce_res.get_i32();
-    let interval = announce_res.get_i32();
-    let leechers = announce_res.get_i32();
-    let seeders = announce_res.get_i32();
-
-    println!("Remaining {}", announce_res.remaining());
-
-    // let ip = announce_res.get_i32();
-    // let port = announce_res.get_i16();
-
-    println!(
-        "action: {}\ntransaction_id: {}\ninterval: {}\nleechers: {}\nseeders: {}",
-        action, transaction_id, interval, leechers, seeders
-    );
-
-    // println!("{}:{}", ip, port);
+    handshake.put_u8(19);
+    handshake.put(&b"BitTorrent protocol"[..]);
+    handshake.put_u32(0);
+    handshake.put_u32(0);
+    handshake.put_slice(&meta_info.info_hash()?);
+    handshake.put_slice(&peer_id);
 
     Ok(())
 }
