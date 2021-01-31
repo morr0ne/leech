f22d5c788a0505fb4c7ce2cdfeafffdb25de3a61
Federico Morrone <contact@morrone.dev>
2021-01-31 12:12:24 +0100

    Improved MetaInfo decoding

--- core/meta_info.rs
+++ core/meta_info.rs
@@ -6,42 +6,78 @@ use bendy::{
 use sha1::{digest::FixedOutput, Digest, Sha1};
 use std::convert::TryInto;
 
+/// Dictionary containg vital information about the torrent
 pub struct MetaInfo {
+    /// The announce url of the tracker.
+    /// According to the specification this is always set.
+    /// In the real world most torrents ditch it in favor of announce list or trackless peers
+    ///
+    /// The url supports http tracking via get requests and udp tracking. It's worth noting that many trackers will accept either protocols regardless of the one specified
     pub announce: Option<String>,
-    pub announce_list: Option<Vec<String>>,
+    /// A list of list of announce urls.
+    pub announce_list: Option<Vec<Vec<String>>>,
+    /// An optional comment about this torrent
     pub comment: Option<String>,
+    /// Name of version of the program used to create the torrent
     pub created_by: Option<String>,
+    /// Time of creation represented in millisecond since [UNIX epoch][`std::time::UNIX_EPOCH`]
     pub creation_date: Option<u64>,
+    /// The encoding format used by [pieces][`Info::SingleFile::pieces`]
     pub encoding: Option<String>,
+    /// Unknown field
     pub http_seeds: Option<Vec<String>>,
+    /// A dictionary containing information about the file(s) of the torrent
     pub info: Info,
 }
 
+/// A dictionary containing information about the file(s) of the torrent
 pub enum Info {
+    /// Information about a single file
     SingleFile {
+        /// Length of the file in bytes
         length: u64,
+        /// MD5 sum of the file
         md5sum: Option<String>,
+        /// The name of the file, respecting this field is not mandatory
         name: String,
+        /// The number of bytes in each piece
         piece_length: u64,
+        /// String consisting of the concatenation of all 20-byte SHA1 hash values, one per piece
         pieces: Vec<u8>,
+        /// When set to 1 clients should only announce their presence via the tracker specified by the torrent
         private: Option<u8>,
+        /// Unknown field
+        source: Option<String>,
     },
+    /// Information about multiple files
     MultiFile {
+        /// A list of dictionaries, each containing information about one file
         files: Vec<File>,
+        /// The name of the directory in which to store the files, respecting this field is not mandatory
         name: String,
-        pieces: Vec<u8>,
+        /// The number of bytes in each piece
         piece_length: u64,
+        /// String consisting of the concatenation of all 20-byte SHA1 hash values, one per piece
+        pieces: Vec<u8>,
+        /// When set to 1 clients should only announce their presence via the tracker specified by the torrent
         private: Option<u8>,
+        /// Unknown field
+        source: Option<String>,
     },
 }
 
+/// Dictionary containing information about a file
 pub struct File {
+    /// Length of the file in bytes
     pub length: u64,
+    /// MD5 sum of the file
     pub md5sum: Option<String>,
+    /// A list where each element corresponds to either a directory name or (in the case of the final element) the filename
     pub path: Vec<String>,
 }
 
 impl MetaInfo {
+    /// Returns the SHA-1 hash of the info dictionary
     pub fn info_hash(&self) -> Result<[u8; 20]> {
         match self.info.to_bencode() {
             Ok(info) => {
@@ -84,16 +120,32 @@ impl FromBencode for MetaInfo {
         let mut dict_dec = object.try_into_dictionary()?;
         while let Some((key, value)) = dict_dec.next_pair()? {
             match key {
-                b"announce" => announce = Some(String::decode_bencode_object(value)?),
-                b"announce-list" => {
-                    announce_list = Some(Vec::<String>::decode_bencode_object(value)?)
+                b"announce" => {
+                    announce = Some(String::decode_bencode_object(value).context("announce")?)
                 }
-                b"comment" => comment = Some(String::decode_bencode_object(value)?),
-                b"created by" => created_by = Some(String::decode_bencode_object(value)?),
-                b"creation date" => creation_date = Some(u64::decode_bencode_object(value)?),
-                b"encoding" => encoding = Some(String::decode_bencode_object(value)?),
-                b"httpseeds" => http_seeds = Some(Vec::decode_bencode_object(value)?),
-                b"info" => info = Some(Info::decode_bencode_object(value)?),
+                b"announce-list" => {
+                    announce_list = Some(
+                        Vec::<Vec<String>>::decode_bencode_object(value)
+                            .context("announce-list")?,
+                    )
+                }
+                b"comment" => {
+                    comment = Some(String::decode_bencode_object(value).context("comment")?)
+                }
+                b"created by" => {
+                    created_by = Some(String::decode_bencode_object(value).context("created by")?)
+                }
+                b"creation date" => {
+                    creation_date =
+                        Some(u64::decode_bencode_object(value).context("creation date")?)
+                }
+                b"encoding" => {
+                    encoding = Some(String::decode_bencode_object(value).context("encoding")?)
+                }
+                b"httpseeds" => {
+                    http_seeds = Some(Vec::decode_bencode_object(value).context("httpseeds")?)
+                }
+                b"info" => info = Some(Info::decode_bencode_object(value).context("info")?),
                 unknown_field => {
                     return Err(DecodingError::unexpected_field(String::from_utf8_lossy(
                         unknown_field,
@@ -124,16 +176,19 @@ impl FromBencode for Info {
     where
         Self: Sized,
     {
+        let mut files = None;
         let mut length = None;
         let mut md5sum = None;
         let mut name = None;
         let mut piece_length = None;
         let mut pieces = None;
         let mut private = None;
+        let mut source = None;
 
         let mut dict_dec = object.try_into_dictionary()?;
         while let Some((key, value)) = dict_dec.next_pair()? {
             match key {
+                b"files" => files = Some(Vec::<File>::decode_bencode_object(value)?),
                 b"length" => length = Some(u64::decode_bencode_object(value)?),
                 b"md5sum" => md5sum = Some(String::decode_bencode_object(value)?),
                 b"name" => name = Some(String::decode_bencode_object(value)?),
@@ -144,6 +199,64 @@ impl FromBencode for Info {
                         .map(|bytes| Some(bytes.0))?;
                 }
                 b"private" => private = Some(u8::decode_bencode_object(value)?),
+                b"source" => source = Some(String::decode_bencode_object(value)?),
+                unknown_field => {
+                    return Err(DecodingError::unexpected_field(String::from_utf8_lossy(
+                        unknown_field,
+                    )));
+                }
+            }
+        }
+
+        let name = name.ok_or_else(|| DecodingError::missing_field("name"))?;
+        let piece_length =
+            piece_length.ok_or_else(|| DecodingError::missing_field("piece_length"))?;
+        let pieces = pieces.ok_or_else(|| DecodingError::missing_field("pieces"))?;
+
+        if let Some(files) = files {
+            Ok(Info::MultiFile {
+                files,
+                name,
+                piece_length,
+                pieces,
+                private,
+                source,
+            })
+        } else {
+            let length = length.ok_or_else(|| DecodingError::missing_field("length"))?;
+            Ok(Info::SingleFile {
+                name,
+                length,
+                md5sum,
+                piece_length,
+                pieces,
+                private,
+                source,
+            })
+        }
+    }
+}
+
+impl FromBencode for File {
+    const EXPECTED_RECURSION_DEPTH: usize = 0;
+
+    fn decode_bencode_object(object: Object) -> Result<Self, DecodingError>
+    where
+        Self: Sized,
+    {
+        let mut length = None;
+        let mut md5sum = None;
+        let mut path = None;
+
+        let mut dict_dec = object.try_into_dictionary()?;
+        while let Some((key, value)) = dict_dec.next_pair()? {
+            match key {
+                b"length" => length = Some(u64::decode_bencode_object(value).context("length")?),
+                b"md5sum" => md5sum = Some(String::decode_bencode_object(value).context("md5sum")?),
+                b"path" => {
+                    path = Some(Vec::<String>::decode_bencode_object(value).context("path")?)
+                }
+
                 unknown_field => {
                     return Err(DecodingError::unexpected_field(String::from_utf8_lossy(
                         unknown_field,
@@ -153,18 +266,12 @@ impl FromBencode for Info {
         }
 
         let length = length.ok_or_else(|| DecodingError::missing_field("length"))?;
-        let name = name.ok_or_else(|| DecodingError::missing_field("name"))?;
-        let piece_length =
-            piece_length.ok_or_else(|| DecodingError::missing_field("piece_length"))?;
-        let pieces = pieces.ok_or_else(|| DecodingError::missing_field("pieces"))?;
+        let path = path.ok_or_else(|| DecodingError::missing_field("path"))?;
 
-        Ok(Info::SingleFile {
-            name,
+        Ok(File {
             length,
             md5sum,
-            piece_length,
-            pieces,
-            private,
+            path,
         })
     }
 }
@@ -214,6 +321,7 @@ impl ToBencode for Info {
                 piece_length,
                 pieces,
                 private,
+                source,
             } => encoder.emit_dict(|mut e| {
                 e.emit_pair(b"length", length)?;
                 if let Some(sum) = md5sum {
@@ -225,6 +333,9 @@ impl ToBencode for Info {
                 if let Some(p) = private {
                     e.emit_pair(b"private", p)?;
                 }
+                if let Some(p) = source {
+                    e.emit_pair(b"source", p)?;
+                }
                 Ok(())
             })?,
             Info::MultiFile {
@@ -233,13 +344,17 @@ impl ToBencode for Info {
                 piece_length,
                 pieces,
                 private,
+                source,
             } => encoder.emit_dict(|mut e| {
                 e.emit_pair(b"files", files)?;
                 e.emit_pair(b"name", name)?;
-                e.emit_pair(b"pieces", AsString(pieces))?;
                 e.emit_pair(b"piece length", piece_length)?;
+                e.emit_pair(b"pieces", AsString(pieces))?;
                 if let Some(p) = private {
                     e.emit_pair(b"private", p)?;
+                }
+                if let Some(p) = source {
+                    e.emit_pair(b"source", p)?;
                 }
                 Ok(())
             })?,
