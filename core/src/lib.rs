83f683b8f326cfd81131f2953714fcb47d491d1d
Federico Morrone <contact@morrone.dev>
2021-07-07 04:33:41 +0200

    Uncommented stuff

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use bytes::BytesMut;
 use metainfo::{bendy::decoding::FromBencode, MetaInfo};
 use tokio::{
     fs,
@@ -10,11 +11,9 @@ pub mod client;
 pub mod utils;
 
 pub use client::Client;
-pub use utils::peer_id;
+pub use utils::{messages::build_handshake, peer_id};
 
 use client::AnnounceRequest;
-
-use crate::utils::messages::build_handshake;
 
 pub async fn start(torrent: &str) -> Result<()> {
     let peer_id = peer_id(b"-LE0001-");
@@ -48,12 +47,7 @@ pub async fn start(torrent: &str) -> Result<()> {
 
         // All the possible messages, see https://wiki.theory.org/BitTorrentSpecification#Messages
         let handshake = build_handshake(&info_hash, &peer_id);
-
-        // const KEEP_ALIVE: [u8; 4] = [0, 0, 0, 0];
-        // const CHOKE: [u8; 5] = [0, 0, 0, 0, 1];
-        // const UNCHOKE: [u8; 5] = [0, 0, 0, 1, 1];
-        // const INTERESTED: [u8; 5] = [0, 0, 0, 1, 2];
-        // const NOT_INTERESTED: [u8; 5] = [0, 0, 0, 1, 3];
+        println!("Handshake: {:?}", handshake);
 
         // Create tcp connection
         // If the connection is refused it probably means this peer is no good
@@ -61,20 +55,20 @@ pub async fn start(torrent: &str) -> Result<()> {
         // but for the sake of simplicity I'll connect just to one for now
 
         println!("Creating tcp stream");
-        let mut stream = TcpStream::connect(peers.as_slice()).await?;
+        let mut stream = TcpStream::connect(peers.as_slice()[78]).await?;
         println!("Connected to {}", stream.peer_addr()?.to_string());
 
-        // let mut buffer = BytesMut::with_capacity(65508);
-        // buffer.resize(65508, 0);
+        let mut buffer = BytesMut::with_capacity(65508);
+        buffer.resize(65508, 0);
 
-        // stream.write(&handshake).await?;
-        // let n = stream.read(&mut buffer).await?;
-        // buffer.truncate(n);
+        stream.write(&handshake).await?;
+        let n = stream.read(&mut buffer).await?;
+        buffer.truncate(n);
 
-        // println!("{:?}", &buffer);
-        // println!("{:?}", &buffer[..]);
-        // println!("{:?}", &buffer.len());
-        // println!("{}", std::str::from_utf8(&buffer[1..20])?);
+        println!("{:?}", &buffer);
+        println!("{:?}", &buffer[..]);
+        println!("{:?}", &buffer.len());
+        println!("{}", std::str::from_utf8(&buffer[1..20])?);
         // stream.read(&mut [0; 128]).await?;
     } else {
         // If no announce url is found it means we should lookup the DHT
