4d425250e361f78a16f8fd0439a2b949e0d521b2
Federico Morrone <contact@morrone.dev>
2021-07-07 03:01:02 +0200

    Improved peer_id performance

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -1,6 +1,5 @@
 use bytes::{BufMut, BytesMut};
 use rand::random;
-use std::convert::TryInto;
 
 // TODO: There is definately a more efficient way to do this
 pub fn peer_id(name: &[u8; 8]) -> [u8; 20] {
@@ -8,8 +7,12 @@ pub fn peer_id(name: &[u8; 8]) -> [u8; 20] {
     peer_id.put(&name[..]);
     peer_id.put(&random::<[u8; 12]>()[..]);
 
-    match peer_id.as_ref().try_into() {
-        Ok(peer_id) => return peer_id,
-        Err(_) => unreachable!(), // This should never be reachable since we know the peer_id size already
-    }
+    // SAFETY: This is safe because we know the lenght of bytes
+    unsafe { slice_to_array(peer_id.as_ref()) }
+}
+
+// SAFETY: The caller must ensure the lenght fits
+pub unsafe fn slice_to_array<T: Copy, const N: usize>(slice: &[T]) -> [T; N] {
+    let ptr = slice.as_ptr() as *const [T; N];
+    *ptr
 }
