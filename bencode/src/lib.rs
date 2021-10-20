de3e3aba6b53907c6f47eb9cb2b7ee0e0b0e4586
Federico morrone <contact@morrone.dev>
2021-10-20 04:26:54 +0200

    Initial bencode implementation

--- bencode/src/lib.rs
+++ bencode/src/lib.rs
@@ -1,1 +1,24 @@
+use indexmap::IndexMap;
+pub mod decode;
 
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum Value<'a> {
+    ByteString(&'a [u8]),
+    Integer(i64),
+    List(Vec<Value<'a>>),
+    Dictionary(IndexMap<&'a [u8], Value<'a>>),
+}
+
+pub struct Decoder {
+    data: Vec<u8>,
+}
+
+impl Decoder {
+    pub fn new(data: Vec<u8>) -> Self {
+        Self { data }
+    }
+}
