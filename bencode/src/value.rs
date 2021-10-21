167dc1c739677b2be858da0d91cd71b7b6c7cfec
Federico morrone <contact@morrone.dev>
2021-10-21 03:30:48 +0200

    Removed serde and added thiserror

--- bencode/src/value.rs
+++ bencode/src/value.rs
@@ -1,9 +1,6 @@
 use indexmap::IndexMap;
-#[cfg(feature = "serde")]
-use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub enum Value<'a> {
     ByteString(&'a [u8]),
     Integer(i64),
