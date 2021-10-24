2c7d6a8c4847d1812992c926b83943511dcab8da
Federico morrone <contact@morrone.dev>
2021-10-24 23:46:59 +0200

    Removed value

--- bencode/src/lib.rs
+++ bencode/src/lib.rs
@@ -1,11 +1,2 @@
 pub mod decode;
 pub mod encode;
-pub mod error;
-
-#[derive(Debug)]
-pub enum Value<'a> {
-    ByteString(&'a [u8]),
-    Integer(i64),
-    List(Vec<Value<'a>>),
-    Dictionary(indexmap::IndexMap<&'a [u8], Value<'a>>),
-}
