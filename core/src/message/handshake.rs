8edfb09aca30659a896797ee221b974fc7617ec8
Federico morrone <contact@morrone.dev>
2021-11-04 19:58:21 +0100

    Moved handshake to its own file

---
+++ core/src/message/handshake.rs
@@ -0,0 +1,46 @@
+use anyhow::{anyhow, bail, Result};
+use array_utils::ToArrayUnchecked;
+use nom::{
+    bytes::complete::take, combinator::map_res, multi::length_data, number::complete::be_u8,
+    sequence::tuple, Finish, IResult,
+};
+
+pub struct Handshake {
+    pub reserved_bytes: [u8; 8],
+    pub info_hash: [u8; 20],
+    pub peer_id: [u8; 20],
+}
+
+impl Handshake {
+    pub fn from_bytes(bytes: &[u8; 68]) -> Result<Self> {
+        let handshake: IResult<&[u8], Self> = map_res(
+            tuple((
+                length_data(be_u8),
+                take(8usize),
+                take(20usize),
+                take(20usize),
+            )),
+            |(pstr, mut reserved_bytes, mut info_hash, mut peer_id): (
+                &[u8],
+                &[u8],
+                &[u8],
+                &[u8],
+            )| {
+                if pstr == b"BitTorrent protocol" {
+                    Ok(Self {
+                        reserved_bytes: unsafe { reserved_bytes.to_array_unchecked() },
+                        info_hash: unsafe { info_hash.to_array_unchecked() },
+                        peer_id: unsafe { peer_id.to_array_unchecked() },
+                    })
+                } else {
+                    bail!("Invalid pstr")
+                }
+            },
+        )(bytes);
+
+        handshake
+            .finish()
+            .map_err(|_| anyhow!("Failed to parse handshake"))
+            .map(|(_, handshake)| handshake)
+    }
+}
