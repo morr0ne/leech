f8d476f23150e896976f14079af0f03deb135196
Federico Maria Morrone <contact@morrone.dev>
2022-06-15 18:33:42 +0200

    Added quotes to unsupported type error

--- bencode/src/error.rs
+++ bencode/src/error.rs
@@ -25,7 +25,7 @@ pub enum Error {
     Utf8(#[from] Utf8Error),
     #[error("")]
     Io(#[from] std::io::Error),
-    #[error("Unsupported type {0}")]
+    #[error("Unsupported type \"{0}\"")]
     Unsupported(&'static str),
 }
 
