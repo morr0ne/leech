899956f50776696e0ec0c6794fd6d1abb0dde76f
Federico Maria Morrone <contact@morrone.dev>
2022-06-24 03:17:11 +0200

    Implemented Deserialize for Value

--- bencode/src/value.rs
+++ bencode/src/value.rs
@@ -1,3 +1,4 @@
+use serde::{de::Visitor, Deserialize};
 use serde_bytes::ByteBuf;
 use std::collections::BTreeMap;
 
@@ -15,6 +16,22 @@ pub struct Integer {
 enum IntegerType {
     Negative(i64),
     Positive(u64),
+}
+
+impl From<i64> for Integer {
+    fn from(value: i64) -> Self {
+        Self {
+            inner: IntegerType::Negative(value),
+        }
+    }
+}
+
+impl From<u64> for Integer {
+    fn from(value: u64) -> Self {
+        Self {
+            inner: IntegerType::Positive(value),
+        }
+    }
 }
 
 impl Integer {
@@ -47,5 +64,92 @@ impl Integer {
             IntegerType::Positive(n) => Some(n),
             IntegerType::Negative(_) => None,
         }
+    }
+}
+
+impl<'de> Deserialize<'de> for Value {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct ValueVisitor;
+
+        impl<'de> Visitor<'de> for ValueVisitor {
+            type Value = Value;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("any valid Bencode value")
+            }
+
+            fn visit_i64<E>(self, value: i64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(Value::Integer(value.into()))
+            }
+
+            fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(Value::Integer(value.into()))
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(Value::ByteString(ByteBuf::from(value)))
+            }
+
+            fn visit_bytes<E>(self, value: &[u8]) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(Value::ByteString(ByteBuf::from(value)))
+            }
+
+            fn visit_some<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                Deserialize::deserialize(deserializer)
+            }
+
+            fn visit_newtype_struct<D>(self, deserializer: D) -> Result<Self::Value, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                Deserialize::deserialize(deserializer)
+            }
+
+            fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::SeqAccess<'de>,
+            {
+                let mut vec = Vec::new();
+
+                while let Some(elem) = seq.next_element()? {
+                    vec.push(elem);
+                }
+
+                Ok(Value::List(vec))
+            }
+
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: serde::de::MapAccess<'de>,
+            {
+                let mut dict = BTreeMap::new();
+
+                while let Some((key, value)) = map.next_entry()? {
+                    dict.insert(key, value);
+                }
+
+                Ok(Value::Dictionary(dict))
+            }
+        }
+
+        deserializer.deserialize_any(ValueVisitor)
     }
 }
