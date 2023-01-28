134b7fae9f752c490dc60604512a112f75573570
Federico Maria Morrone <contact@morrone.dev>
2023-01-28 22:25:22 +0100

    Skip deserializing None variants

--- core/src/meta_info.rs
+++ core/src/meta_info.rs
@@ -15,25 +15,30 @@ pub struct MetaInfo {
     /// In the real world most torrents ditch it in favor of announce list or trackless peers
     ///
     /// The url supports http tracking via get requests and udp tracking. It is worth noting that many trackers will accept either protocols regardless of the one specified
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub announce: Option<Url>,
     /// A list of list of announce urls.
-    #[serde(rename = "announce-list")]
+    #[serde(rename = "announce-list", skip_serializing_if = "Option::is_none")]
     pub announce_list: Option<Vec<Vec<Url>>>,
     /// An optional comment about this torrent
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub comment: Option<String>,
     /// Name of version of the program used to create the torrent
-    #[serde(rename = "created by")]
+    #[serde(rename = "created by", skip_serializing_if = "Option::is_none")]
     pub created_by: Option<String>,
     /// Time of creation represented in millisecond since [UNIX epoch][`std::time::UNIX_EPOCH`]
-    #[serde(rename = "creation date")]
+    #[serde(rename = "creation date", skip_serializing_if = "Option::is_none")]
     pub creation_date: Option<u64>,
     /// The encoding format used by [pieces][`Info::pieces`]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub encoding: Option<String>,
     /// Unknown field
+    #[serde(rename = "httpseeds", skip_serializing_if = "Option::is_none")]
     pub http_seeds: Option<Vec<String>>,
     /// A dictionary containing information about the file(s) of the torrent
     pub info: Info,
     // TODO: docs
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub url_list: Option<Vec<Url>>,
 }
 
@@ -48,8 +53,10 @@ pub struct Info {
     #[serde(with = "serde_bytes")]
     pub pieces: Vec<u8>,
     /// When set to 1 clients should only announce their presence via the tracker specified by the torrent
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub private: Option<u8>,
     /// Unknown field
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub source: Option<String>,
     #[serde(flatten)]
     pub files: FileKind,
@@ -70,6 +77,7 @@ pub enum FileKind {
         /// Length of the file in bytes
         length: u64,
         /// MD5 sum of the file
+        #[serde(skip_serializing_if = "Option::is_none")]
         md5sum: Option<String>,
     },
 }
@@ -80,6 +88,7 @@ pub struct File {
     /// Length of the file in bytes
     pub length: u64,
     /// MD5 sum of the file
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub md5sum: Option<String>,
     /// A list where each element corresponds to either a directory name or (in the case of the final element) the filename
     pub path: Vec<String>,
