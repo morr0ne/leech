89c0fe7e111bcfc38029b7af932f84966975638d
Federico Maria Morrone <contact@morrone.dev>
2023-01-28 21:30:33 +0100

    Inlined all docs

--- bencode/src/lib.rs
+++ bencode/src/lib.rs
@@ -12,8 +12,13 @@ mod error;
 mod ser;
 pub mod value;
 
+#[doc(inline)]
 pub use byte_string::ByteString;
+#[doc(inline)]
 pub use de::{from_bytes, Deserializer};
+#[doc(inline)]
 pub use error::{Error, Result};
+#[doc(inline)]
 pub use ser::{to_bytes, to_writer, Serializer};
+#[doc(inline)]
 pub use value::{Integer, Value};
